ceb96a9a4524f67df755bcd913ba13b7501aafa7
Dominik <32032778+dommyrock@users.noreply.github.com>
2019-02-11 22:36:30 +0100

    added new Zmq test chat app
    Client-Server /REQ -REP socket message queue with ZMQ

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ZeroMQTestApps
+Used documentation: http://zguide.zeromq.org/
+
+This is basic client- server / REQ -REP chat app made in .NET windows forms  using ZMQ library . 
+1) We bind server/REP zsocket (localhost + any(free) port) by pressing connect btn
+2) We connect client /REQ zsocket to server /REP socket 
+2.1)client sends $$$$ClientREADY$$$$ frame to zsocket
+2.2)Server receives $$$$ClientREADY$$$$ frame on zsocket and sends $$$$ServerREADY$$$$ frame
+.... process repeats in the background on separate thereads
+
+**IMPORTANT ***  if Server Unbinds all msgs by client will never be received by server (even on server re-bind)
+                 if Client Disconnects , server sends messages, client will receive all messages
+sent by server while client was disconnected !!!
+---------------------------------------------------
---
+++ ZeroMQ_ReqRep/DotMetrics.TestChat/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZeroMQ;
+
+namespace DotMetrics.TestChat
+{
+    public partial class WindowsLiveMessenger : Form
+    {
+        private bool isServer = true;
+        private string endpoint = "tcp://";
+        private ChatNode cNode;
+        private string uiTxt = "";
+
+        public WindowsLiveMessenger()
+        {
+            InitializeComponent();
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            if (isServer)
+            {
+                uiTxt = "Server:     " + sendTxtbox.Text;
+            }
+            else
+            {
+                uiTxt = "Client:       " + sendTxtbox.Text;
+            }
+
+            sendTxtbox.Clear();
+
+            Message msg = new Message(DateTime.Now, uiTxt);
+            if (cNode != null)
+            {
+                cNode.InputTextFromUi(msg);
+
+                cNode.OutputTextToUi(msg);
+            }
+        }
+
+        private void connectionBtn_Click(object sender, EventArgs e)
+        {
+            isServer = checkBox1.Checked;
+            endpoint += IPtext.Text;
+            if (cNode == null)
+                cNode = new ChatNode(isServer);
+            cNode.Start(endpoint);
+            timer.Start(); //refreshes output lists every 100ms
+        }
+
+        private void disconnectBtn_Click(object sender, EventArgs e)
+        {
+            if (cNode != null)
+                cNode.Stop(endpoint);
+            endpoint = "tcp://";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            receiveTxtbox.Text = cNode.RefreshOutput();
+            receiveTxtbox.SelectionStart = receiveTxtbox.Text.Length;
+            receiveTxtbox.ScrollToCaret();
+        }
+
+        private void sendTxtbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                sendBtn_Click(this, new EventArgs());
+            }
+        }
+
+        private void btn_Quit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+    }
+}
